6f34602e7e1cd6003233059b1e50ee373eaa9f51
nurramdandoni <nurramdandoni@gmail.com>
2019-09-07 15:24:56 +0700

    custom user sidebar

--- application/views/template_layout/sidebar_menu.php
+++ application/views/template_layout/sidebar_menu.php
@@ -542,42 +542,32 @@
                 <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="0px,0px">
                 <div class="kt-header__topbar-user">
                 <span class="kt-header__topbar-welcome kt-hidden-mobile">Hi,</span>
-                <span class="kt-header__topbar-username kt-hidden-mobile">Sean</span>
+                <span class="kt-header__topbar-username kt-hidden-mobile">Admin</span>
                 <img class="kt-hidden" alt="Pic" src="<?php echo base_url(); ?>template/assets2/media/users/300_25.jpg" />
                 <!--use below badge element instead the user avatar to display username's first letter(remove kt-hidden class to display it) -->
                 <span class="kt-badge kt-badge--username kt-badge--unified-success kt-badge--lg kt-badge--rounded kt-badge--bold">S</span>
                 </div>
                 </div>
 
-                <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-top-unround dropdown-menu-xl">
+                <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-top-unround dropdown-menu-xl" >
                 <!--begin: Head -->
-                <div class="kt-user-card kt-user-card--skin-dark kt-notification-item-padding-x" style="background-image: url(<?php echo base_url(); ?>template/assets2/media/misc/bg-1.jpg)">
+                <div class="kt-user-card kt-user-card--skin-dark kt-notification-item-padding-x" style="background-image: url(<?php echo base_url(); ?>template/assets2/media/misc/bg-1.jpg)" style="background-color: #DF3338;">
                 <div class="kt-user-card__avatar">
                 <img class="kt-hidden" alt="Pic" src="<?php echo base_url(); ?>template/assets2/media/users/300_25.jpg" />
                 <!--use below badge element instead the user avatar to display username's first letter(remove kt-hidden class to display it) -->
-                <span class="kt-badge kt-badge--lg kt-badge--rounded kt-badge--bold kt-font-success">S</span>
+                <span class="kt-badge kt-badge--lg kt-badge--rounded kt-badge--bold kt-font-success">A</span>
                 </div>
                 <div class="kt-user-card__name">
-                Sean Stone
+                Administrator
                 </div>
                 <div class="kt-user-card__badge">
-                <span class="">Hai, Sean</span>
+                <span class="">Hi, Admin</span>
                 </div>
                 </div>
                 <!--end: Head -->
 
                 <!--begin: Navigation -->
                 <div class="kt-notification">
-                <a href="demo1/custom/apps/user/profile-1/personal-information.html" class="kt-notification__item">
-                <div class="kt-notification__item-icon">
-                <i class="flaticon2-calendar-3 kt-font-success"></i>
-                </div>
-                <div class="kt-notification__item-details">
-                <div class="kt-notification__item-title kt-font-bold">
-                    My Profile
-                </div>
-                </div>
-                </a>
                 
                 <div class="kt-notification__custom kt-space-between">
 
